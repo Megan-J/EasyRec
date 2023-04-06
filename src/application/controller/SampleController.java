9c71c0adf6c15f26a748bb11eb25521647ea6557
megan <megan@localhost>
2023-04-06 00:09:46 -0700

    in theory, this should now be the most updated version; btn issue still there

--- src/application/controller/SampleController.java
+++ src/application/controller/SampleController.java
@@ -1,4 +1,4 @@
-package application;
+package application.controller;
 
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -49,4 +49,12 @@ public class SampleController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void logout(ActionEvent event) throws IOException{
+ 		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+ 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+ 		scene = new Scene(root);
+ 		stage.setScene(scene);
+ 		stage.show();
+ 	}
 }
