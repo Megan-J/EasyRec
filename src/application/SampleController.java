068919ff3883287cdaa83debcbbd21548fbb32d8
megan <megan@localhost>
2023-04-05 23:46:10 -0700

    adding Jonathan's pr code here; initial error with btn still here

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -18,7 +18,7 @@ public class SampleController {
 	
 	public void switchToScene1(ActionEvent event) throws IOException {
 
-		Parent root = FXMLLoader.load(getClass().getResource("gui/Scene1.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
@@ -27,7 +27,23 @@ public class SampleController {
 	
 	public void switchToCreateRec(ActionEvent event) throws IOException{
 
-		Parent root = FXMLLoader.load(getClass().getResource("gui/CreateRecommendation.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("CreateRecommendation.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToLogin(ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToProfile(ActionEvent event) throws IOException{
+		Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
