9ce092fb58b29ad6d27964c520b45023af6d5307
Tyler-Dunning <118504218+Tyler-Dunning@users.noreply.github.com>
2023-04-06 13:24:46 -0700

    Add files via upload

---
+++ src/application/ProfileController.java
@@ -0,0 +1,10 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+
+
