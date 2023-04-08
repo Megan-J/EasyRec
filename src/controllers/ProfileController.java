3071ed7902fb86e7148f3c4ca2df6965ad24d3fb
Tyler-Dunning <118504218+Tyler-Dunning@users.noreply.github.com>
2023-04-08 16:27:00 -0700

    Update ProfileController.java

--- src/controllers/ProfileController.java
+++ src/controllers/ProfileController.java
@@ -18,7 +18,6 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
